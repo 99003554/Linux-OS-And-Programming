eb58ebcbc2b5f45e32216a604e1ed806e13b365a
abhishekreddy012935 <38652723+abhishekreddy012935@users.noreply.github.com>
2021-03-02 14:38:53 +0530

    Updated

--- Assignment-1/src/mystring.c
+++ Assignment-1/src/mystring.c
@@ -1,21 +1,32 @@
 #include "mystring.h"
 
-
+/**
+    This function will return length of the string
+**/
 size_t mystrlen(const char *str)
 {
     return strlen(str);
 }
 
+/**
+    This function is used to copy the string into another
+**/
 char* mystrcpy(char* str1, const char* str2)
 {
     return strcpy(str1,str2);
 }
 
+/**
+    This function is used to concatinate the string into another
+**/
 char* mystrcat(char *str1, const char *str2)
 {
     return strcat(str1,str2);
 }
 
+/**
+    This function is used to compare the string into another
+**/
 int mystrcmp(const char *str1, const char *str2 )
 {
     return strcmp(str1,str2);
